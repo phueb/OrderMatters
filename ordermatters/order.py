acedfbd5d08a822b57cd3f93d1a6877af653103e
phueb <info@philhuebner.com>
2019-11-03 09:42:10 -0600

    added drafts of scripts

--- ordermatters/order.py
+++ ordermatters/order.py
@@ -1,12 +1,41 @@
-import numpy as np
+from typing import List
 
 
-def order_pseudo_randomly(self, parts):
-    idx = []
-    for i, j in zip(np.roll(np.arange(self.params.num_parts), self.params.num_parts // 2)[::2],
-                    np.roll(np.arange(self.params.num_parts), self.params.num_parts // 2)[::-2]):
-        idx += [i, j]
-    assert len(set(idx)) == len(parts)
-    res = [parts[i] for i in idx]
-    assert len(res) == len(parts)
+def reorder_docs_from_midpoint(docs: List[str]
+                               ) -> List[str]:
+    """
+    reorder docs such that first docs are docs that are most central
+    """
+    # start, middle, end
+    s = 0
+    e = len(docs)
+    m = e // 2
+
+    res = []
+    for i, j in zip(range(m, e + 1)[::+1],
+                    range(s, m + 0)[::-1]):
+        res += [docs[i], docs[j]]
+
+    assert len(res) == len(docs)
+
+    return res
+
+
+def reorder_docs_from_ends(docs: List[str]
+                           ) -> List[str]:
+    """
+    reorder docs such that first docs are docs that are from ends
+    """
+    # start, middle, end
+    s = 0
+    e = len(docs)
+    m = e // 2
+
+    res = []
+    for i, j in zip(range(m, e + 0)[::-1],
+                    range(s, m + 1)[::+1]):
+        res += [docs[i], docs[j]]
+
+    assert len(res) == len(docs)
+
     return res
