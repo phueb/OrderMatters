7d8eda8f75f3b717d61ddd15cc02508ecdadc507
phueb <info@philhuebner.com>
2020-02-18 10:24:15 -0600

    add joint entropy

--- compare_orderings.py
+++ compare_orderings.py
@@ -1,21 +1,24 @@
 from scipy.stats import spearmanr
+import random
 
 from preppy import PartitionedPrep
 from preppy.docs import load_docs
 
 from ordermatters import config
 from ordermatters.reorder import reorder_by_conditional_entropy
+from ordermatters.reorder import reorder_by_joint_entropy
 
-NUM_PARTS = 32
+NUM_PARTS = 2
 CORPUS_NAME = 'childes-20191206'
-PROBES_NAME = 'nouns-2972'
-
+PROBES_NAME = 'verbs-1321'
+PROBES_NAME = 'nouns-2972'  # TODO use sem-4096
+NUM_SKIP_FIRST_DOCS = 0
 
 corpus_path = config.Dirs.corpora / f'{CORPUS_NAME}.txt'
 docs, _ = load_docs(corpus_path, num_test_docs=0)
-prep = PartitionedPrep(docs,
+prep = PartitionedPrep(docs[NUM_SKIP_FIRST_DOCS:],
                        reverse=False,
-                       num_types=None,
+                       num_types=4096,
                        num_parts=NUM_PARTS,
                        num_iterations=[1, 1],
                        batch_size=64,
@@ -24,14 +27,19 @@ prep = PartitionedPrep(docs,
 # load a category of words, X, for which to compute conditional entropy, H(X|Y)
 probes_file_path = config.Dirs.words / f'{CORPUS_NAME}-{PROBES_NAME}.txt'
 probes = [w for w in probes_file_path.read_text().split('\n') if w in prep.store.w2id]
+print('num probes', len(probes))
 
 # input to spearman correlation
 ordered_part_ids = [n for n in range(prep.num_parts)]
-reordered_part_ids = reorder_by_conditional_entropy(prep, probes)
-print(reordered_part_ids)
+reordered_part_ids_ce = reorder_by_conditional_entropy(prep, probes)
+reordered_part_ids_je = reorder_by_joint_entropy(prep, probes)
 
+print('conditional entropy:')
+rho, p_value = spearmanr(ordered_part_ids, reordered_part_ids_ce)
+print(f'rho={rho: .4f}')
+print(f'p-v={p_value: .4f}')
 
-# noinspection PyTypeChecker
-rho, p_value = spearmanr(ordered_part_ids, reordered_part_ids)
-print(f'rho={rho:.4f}')
-print(f'p-v={p_value:.4f}')
+print('joint entropy:')
+rho, p_value = spearmanr(ordered_part_ids, reordered_part_ids_je)
+print(f'rho={rho: .4f}')
+print(f'p-v={p_value: .4f}')
--- ordermatters/reorder.py
+++ ordermatters/reorder.py
@@ -29,8 +29,46 @@ def reorder_by_conditional_entropy(prep: PartitionedPrep,
         # conditional entropy
         x = probe_windows[:, -2]  # CAT member
         y = probe_windows[:, -1]  # next-word
-        ces.append(drv.entropy_conditional(x, y))
+        ces.append(drv.entropy_conditional(x, y).item())
 
     # get indices that sort conditional entropies from highest to lowest H(X|Y)
     res = list(sorted(range(prep.num_parts), key=lambda i: ces[i], reverse=True))
+
+    # TODO Debugging
+    print([round(ces[i], 2) for i in res])
+
+    return res
+
+
+def reorder_by_joint_entropy(prep: PartitionedPrep,
+                                   probes: List[str]) -> List[int]:
+    """
+    1. compute conditional entropy of probe words, given distribution of words that follow them, for each partition.
+    2. re-order partitions from high to low conditional entropy.
+    """
+    # calc H(X|Y) for each part
+    ces = []
+    for part in prep.reordered_parts:
+        # windows
+        token_ids_array = part.astype(np.int64)
+        num_possible_windows = len(token_ids_array) - prep.num_tokens_in_window
+        shape = (num_possible_windows, prep.num_tokens_in_window)
+        windows = as_strided(token_ids_array, shape, strides=(8, 8), writeable=False)
+
+        # windows with probe in position -2
+        row_ids = np.isin(windows[:, -2], [prep.store.w2id[w] for w in probes])
+        probe_windows = windows[row_ids]
+
+        # conditional entropy
+        x = probe_windows[:, -2]  # CAT member
+        y = probe_windows[:, -1]  # next-word
+        x_y = np.vstack((x, y))
+        ces.append(drv.entropy_joint(x_y).item())
+
+    # get indices that sort conditional entropies from highest to lowest H(X|Y)
+    res = list(sorted(range(prep.num_parts), key=lambda i: ces[i], reverse=True))
+
+    # TODO Debugging
+    print([round(ces[i], 2) for i in res])
+
     return res
